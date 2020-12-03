ae653b7aefd2c6a1e32353db086f681e8c0d6515
Minh Nguyen <minh.nguyen@insee.fr>
2020-12-03 09:25:36 +0100

    tp json

--- struts2-sndio/src/main/java/fr/insee/cnio/formation/struts/action/AffichageAction.java
+++ struts2-sndio/src/main/java/fr/insee/cnio/formation/struts/action/AffichageAction.java
@@ -1,26 +1,31 @@
 package fr.insee.cnio.formation.struts.action;
 
-import java.util.Date;
+import java.util.List;
 
 import com.opensymphony.xwork2.ActionSupport;
+
+import fr.insee.cnio.formation.struts.modele.Personne;
+import fr.insee.cnio.formation.struts.service.PersonneService;
 
 public class AffichageAction extends ActionSupport {
 
 	private static final long serialVersionUID = 1L;
 	
-	private Date date;
+	private List<Personne> listePersonnes;
 	
-	public String afficher() {
-		date = new Date();
-		return SUCCESS;
+	public String lister() {
+		PersonneService personneService = new PersonneService();
+		listePersonnes = personneService.getToutLeMonde();
+		return NONE;
 	}
 
-	public Date getDate() {
-		return date;
+
+	public List<Personne> getListePersonnes() {
+		return listePersonnes;
 	}
 
-	public void setDate(Date date) {
-		this.date = date;
+	public void setListePersonnes(List<Personne> listePersonnes) {
+		this.listePersonnes = listePersonnes;
 	}
 
 }
