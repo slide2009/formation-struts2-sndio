3f1a5277614dbd04d794b3b3ad321181be4d1945
Minh Nguyen <minh.nguyen@insee.fr>
2020-12-04 08:08:43 +0100

    tp json

--- struts2-sndio/src/main/java/fr/insee/cnio/formation/struts/action/JsonAction.java
+++ struts2-sndio/src/main/java/fr/insee/cnio/formation/struts/action/JsonAction.java
@@ -7,15 +7,22 @@ import com.opensymphony.xwork2.ActionSupport;
 import fr.insee.cnio.formation.struts.modele.Personne;
 import fr.insee.cnio.formation.struts.service.PersonneService;
 
-public class AffichageAction extends ActionSupport {
+public class JsonAction extends ActionSupport {
 
 	private static final long serialVersionUID = 1L;
 	
 	private List<Personne> listePersonnes;
+	private Personne personne;
+	private String nom;
+	private PersonneService personneService = new PersonneService();
 	
 	public String lister() {
-		PersonneService personneService = new PersonneService();
 		listePersonnes = personneService.getToutLeMonde();
+		return SUCCESS;
+	}
+	
+	public String afficherDetail() {
+		personne = personneService.chercherPersonneParNom(nom);
 		return NONE;
 	}
 
@@ -26,6 +33,22 @@ public class AffichageAction extends ActionSupport {
 
 	public void setListePersonnes(List<Personne> listePersonnes) {
 		this.listePersonnes = listePersonnes;
+	}
+
+	public Personne getPersonne() {
+		return personne;
+	}
+
+	public void setPersonne(Personne personne) {
+		this.personne = personne;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
 	}
 
 }
